cd2cb58a79499a1d70ce2477c6c2cf2f44e4de91
Rafael Waldo Delgado Doblas <lord.rafa@gmail.com>
2022-05-29 18:36:03 +0200

    Solving detection issue

--- bin/dsview-onDocker.sh
+++ bin/dsview-onDocker.sh
@@ -6,16 +6,12 @@ mkdir -p "$LOCAL_HOME" 2> /dev/null
 mkdir -p "$LOCAL_WORKSPACE" 2> /dev/null
 
 OPTS=""
-
-# Uncomment following line for GUI support
 OPTS="${OPTS} --env=DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix"
-
-# Uncomment following lines for USB support (replace XXX by USB device mayor number) 
-# OPTS="${OPTS} --device /dev/kvm --group-add kvm"
-# OPTS="${OPTS} -v /dev/bus/usb:/dev/bus/usb:ro --group-add plugdev --device-cgroup-rule 'c XXX:* rwm'"
 
 docker run --rm -i \
 	${OPTS} \
+	-v /dev/bus/usb:/dev/bus/usb:ro --device-cgroup-rule 'c 189:* rwm' \
+    -net host -v /run/udev/control:/run/udev/control \
 	-v "$LOCAL_HOME:/App_Home" \
 	-v "$LOCAL_WORKSPACE:/App_Home/dsview" \
 	lordrafa/dsview $@
