f685e16d997f62b40168328f62854209feed36c1
Rafael Waldo Delgado Doblas <lord.rafa@gmail.com>
2022-06-21 02:21:43 +0200

    Fixing desktop run

--- bin/dsview-onDocker.sh
+++ bin/dsview-onDocker.sh
@@ -10,7 +10,7 @@ CONTAINER_NAME="dsview-on-docker"
 OPTS=""
 OPTS="${OPTS} --env=DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix"
 
-docker run --rm -it --name="$CONTAINER_NAME" \
+docker run --rm -i --name="$CONTAINER_NAME" \
 	${OPTS} \
 	-v /dev/null:/dev/bus/usb/099 \
 	--device-cgroup-rule 'c 189:* rwm' \
